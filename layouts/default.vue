308fc4002f8eb36e85c204bffa5ed1313061d31a
Minh Hieu <minhhieuu26@gmail.com>
2023-07-18 17:17:19 +0700

    init project

---
+++ layouts/default.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav class="navbar">
+    <NuxtLink class="navbar-item" to="/">Home Page</NuxtLink>
+  </nav>
+  <main>
+    <slot />
+  </main>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.navbar {
+  display: flex;
+  justify-content: center;
+  margin-top: 40px;
+
+  .navbar-item {
+    padding: 8px 16px;
+    border: 1px solid #dbdbdb;
+    border-radius: 4px;
+    text-decoration: none;
+    color: black;
+  }
+}
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  css:[
+    "~/assets/style/global.scss",
+  ],
+  runtimeConfig: {
+    apiKey: '',
+    apiBaseUrl: '',
+  }
+})
